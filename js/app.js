c5f4062713ba5a50350a11537954989bb3431683
SawMinThet <sawminthetdrfeedagentmlm@gmail.com>
2021-09-09 01:12:10 +0630

    finish

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log("Hello Web Developing");
+
+let x = document.getElementById("test");
+let y = document.getElementById("name");
+function testing(){
+    let z = x.value;
+    y.innerHTML = z;
+
+}
+
